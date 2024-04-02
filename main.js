bba4d4c61d2d8b94017ba106aa1c9c35075c6613
IbrahimKhan88 <98701154+IbrahimKhan88@users.noreply.github.com>
2024-04-02 16:15:03 -0700

    Create main.js
    The main file has now been created and can add two integers

---
+++ main.js
@@ -0,0 +1,7 @@
+let firstNumber = parseInt(prompt("Enter the first integer:"));
+
+let secondNumber = parseInt(prompt("Enter the second integer:"));
+
+let sum = firstNumber + secondNumber;
+
+console.log("The sum of", firstNumber, "and", secondNumber, "is:", sum);
